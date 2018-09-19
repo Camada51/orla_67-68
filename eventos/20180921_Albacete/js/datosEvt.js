b9d0e14e20b48aecdd98aa38b1d047a8b5ddcfdc
Camada51 <37265990+Camada51@users.noreply.github.com>
2018-09-19 08:06:21 +0200

    Add files via upload

--- eventos/20180921_Albacete/js/datosEvt.js
+++ eventos/20180921_Albacete/js/datosEvt.js
@@ -6,7 +6,7 @@ tituloEvt = "Reunión en Albacete";
 fechaEvt = new Date("2018", "8", "21"); //año,mes,día (al mes hay que restarle 1, enero =0)
 poblacionEvt = "Albacete";
 localEvt = "Restaurante El Callejón, c/ Guzmán el Bueno 18";
-asistentesEvt = "Antonino de Mora Montes,Carlos Hornillos,Clemente Úbeda,Eduardo Albarracín,Francisco Llorca,José María Pertusa,Juan Diego Pérez Pérez,Rafael. Ladrón de Guevara,Ramón Peyró";
+asistentesEvt = "Eduardo Albarracín,Antonino de Mora Montes, Facundo Giménez Mena,Carlos Hornillos Blasco,Clemente Úbeda,Luis Torres,Rafael Ladrón de Guevara,Francisco Llorca,José María Pertusa,Juan Diego Pérez Pérez,Ramón Peyró, Bartolomé Sánchez";
 fotosEvt = "ElCallejon_Google.jpg,Chapulines_2018-09-16 at 19.44.59.jpeg";
 
 menuEvt = "Por ahora es sorpresa.<br/><br/>Hay quien ha prometido una esquisitez: \"Chapulines\", ya se verá.";
