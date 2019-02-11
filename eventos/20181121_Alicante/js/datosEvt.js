3aa2156d70ab91ed8b043eb3baf1fcbddb5b8c19
Camada51 <chornillos@gmail.com>
2019-02-11 21:54:39 +0100

    actualización Murcia 8/2/2019

---
+++ eventos/20181121_Alicante/js/datosEvt.js
@@ -0,0 +1,12 @@
+﻿//datos del evento de Alicante del 21/11/2018
+
+nombreEvt = "20181121_Alicante";
+directorioEvt = "20181121_Alicante";
+tituloEvt = "Reunión en Alicante";
+fechaEvt = new Date("2018", "10", "21"); //año,mes,día (al mes hay que restarle 1, enero =0)
+poblacionEvt = "Alicante";
+localEvt = "Restaurante Club Montemar,Carrer Virgili, nº 25";
+asistentesEvt = "";
+fotosEvt = "clubMontemar.jpg,P1030378.JPG,2018-11-21 at 19.40.54.jpeg,2018-11-21 at 19.40.55.jpeg,2018-11-21 at 19.40.55_01.jpeg,2018-11-21 at 19.40.55_02.jpeg,2018-11-21 at 19.40.55_03.jpeg,2018-11-21 at 19.40.56.jpeg";
+
+menuEvt = "Sin datos";
---
+++ eventos/20190208_Murcia/js/datosEvt.js
@@ -0,0 +1,16 @@
+﻿//datos del evento de Murcia del 08/02/2019
+
+nombreEvt = "20190208_Murcia";
+directorioEvt = "20190208_Murcia";
+tituloEvt = "Reunión en Murcia";
+fechaEvt = new Date("2019", "1", "8"); //año,mes,día (al mes hay que restarle 1, enero =0)
+poblacionEvt = "Murcia";
+localEvt = "Restaurante La Pequeña Taberna, C/ General Margallo";
+asistentesEvt = "Rafael Ladrón de Guevara,Antonino de Mora,Salvador Ros,Diego Soler,Vicente Anguíz,José María Pertusa,Carlos Hornillos,Luis Torres,Pedro Revenga,Juan Puigcerver,Manolo Guillén,Pepe Alós,Ramón Sirvent,José Manuel Maciá,Julio Delgado,José Luis Alonso,Bartolomé Sánchez,Facundo Giménez";
+fotosEvt = "paseo_2,6Km.png,LaPequeTaberna.jpg,P1030406.JPG,P1030407.JPG,2019-02-08 at 13.33.19.jpeg,P1030410.JPG,2019-02-08 at 14.20.09_0.jpeg,P1030411.JPG,2019-02-08 at 14.20.09_01.jpeg,P1030412.JPG,2019-02-08 at 14.20.09_02.jpeg,P1030413.JPG,2019-02-08 at 14.20.09_03.jpeg,P1030414.JPG,2019-02-08 at 14.20.09_04.jpeg,P1030415.JPG,2019-02-08 at 14.20.09_05.jpeg,P1030416.JPG,2019-02-08 at 14.20.09_06.jpeg,P1030417.JPG,2019-02-08 at 14.20.09_07.jpeg,P1030418.JPG,2019-02-08 at 14.20.09_08.jpeg,P1030419.JPG,2019-02-08 at 14.20.09_09.jpeg,P1030420.JPG,2019-02-08 at 14.20.10_01.jpeg,P1030421.JPG,2019-02-08 at 14.20.10_02.jpeg,P1030422.JPG,2019-02-08 at 14.20.10_03.jpeg,P1030423.JPG,2019-02-08 at 14.20.10_04.jpeg,P1030424.JPG,2019-02-08 at 17.48.39.jpeg";
+
+menuEvt = "La mañana comenzó con un agradable paseo de más de 2 kilometros por los sitios más destacados de Murcia. De la mano de José Luis Alonso recorrimos la plaza de Santo Domingo, calle Traperias, Calle Platerías, Plaza de las Flores, Palacio Almuní, Paseo del Malecón, Ayuntamiento, Catedral y terminamos muertos de hambre en La Pequeña"+
+"El menú:<br/><br/>" +
+    "&nbsp;&nbsp;&nbsp; Aperitivos diversos de la tierra<br/>" +
+    "&nbsp;&nbsp;&nbsp; Lomo de bacalao o Dorada o Solomillo<br/>" +
+    "&nbsp;&nbsp;&nbsp; Paparojotes y mini gintonic para terminar<br/>";
