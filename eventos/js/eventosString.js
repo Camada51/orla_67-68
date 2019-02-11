4ce69b505d91cf57f40d70d818703a8a4d2d3e5d
Camada51 <chornillos@gmail.com>
2019-02-11 21:54:39 +0100

    actualización Murcia 8/2/2019

--- eventos/js/eventosString.js
+++ eventos/js/eventosString.js
@@ -1,5 +1,7 @@
 ﻿var eventosString;
 eventosString =
-    "Cincuentenario promoción 67-68 del Colegio de la Incmaculada,20180217_Alicante" + ";"+
-    "Reunión en Madrid,20180525_Madrid" + ";" +
-    "Reunión en Albacete,20180921_Albacete";
+    "Cincuentenario promoción 67-68 del Colegio de la Incmaculada (17/02/2018),20180218_Alicante" + ";"+
+    "Reunión en Madrid (25/05/2018),20180525_Madrid" + ";" +
+    "Reunión en Albacete (21/09/2018),20180921_Albacete" + ";" +
+	"Reunión en Alicante (21/11/2018),20181121_Alicante" + ";" +
+	"Reunión en Murcia (08/02/2019),20190208" ;
