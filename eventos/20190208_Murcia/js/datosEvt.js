bc7bdada6ea5504302bd3d4f590f1b75848dec48
Camada51 <37265990+Camada51@users.noreply.github.com>
2019-02-12 20:33:52 +0100

    Add files via upload

--- eventos/20190208_Murcia/js/datosEvt.js
+++ eventos/20190208_Murcia/js/datosEvt.js
@@ -11,7 +11,8 @@ asistentesEvt = "Alonso, José Luis;Alós, Pepe;Anguíz, Vicente;Delgado, Julio;
 
 fotosEvt = "paseo_2-6Km.png,LaPequeTaberna.jpg,P1030406.JPG,P1030407.JPG,2019-02-08 at 13.33.19.jpeg,P1030410.JPG,2019-02-08 at 14.20.09_0.jpeg,P1030411.JPG,2019-02-08 at 14.20.09_01.jpeg,P1030412.JPG,2019-02-08 at 14.20.09_02.jpeg,P1030413.JPG,2019-02-08 at 14.20.09_03.jpeg,P1030414.JPG,2019-02-08 at 14.20.09_04.jpeg,P1030415.JPG,2019-02-08 at 14.20.09_05.jpeg,P1030416.JPG,2019-02-08 at 14.20.09_06.jpeg,P1030417.JPG,2019-02-08 at 14.20.09_07.jpeg,P1030418.JPG,2019-02-08 at 14.20.09_08.jpeg,P1030419.JPG,2019-02-08 at 14.20.09_09.jpeg,P1030420.JPG,2019-02-08 at 14.20.10_01.jpeg,P1030421.JPG,2019-02-08 at 14.20.10_02.jpeg,P1030422.JPG,2019-02-08 at 14.20.10_03.jpeg,P1030423.JPG,2019-02-08 at 14.20.10_04.jpeg,P1030424.JPG,2019-02-08 at 17.48.39.jpeg";
 
-menuEvt = "La mañana comenzó con un agradable paseo de más de 2 kilometros por los sitios más destacados de Murcia.<br>"+
+menuEvt = "La mañana comenzó con un agradable paseo de más de 2 kilometros por los sitios<br/>" +
+    "más destacados de Murcia.<br>" +
 	"De la mano de José Luis Alonso recorrimos la plaza de Santo Domingo,<br/>"+
 	"Calle Traperias, Calle Platerías, Plaza de las Flores, Palacio Almuní, Paseo del Malecón,<br/>"+
 	"Ayuntamiento, Catedral y terminamos muertos de hambre en La Pequeña."+
