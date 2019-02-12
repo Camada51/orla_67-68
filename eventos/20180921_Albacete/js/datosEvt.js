9123750a1bc47e1264dab553450a0b5f6a7898dc
Camada51 <37265990+Camada51@users.noreply.github.com>
2019-02-12 14:27:46 +0100

    Add files via upload

--- eventos/20180921_Albacete/js/datosEvt.js
+++ eventos/20180921_Albacete/js/datosEvt.js
@@ -6,7 +6,7 @@ tituloEvt = "Reunión en Albacete";
 fechaEvt = new Date("2018", "8", "21"); //año,mes,día (al mes hay que restarle 1, enero =0)
 poblacionEvt = "Albacete";
 localEvt = "Restaurante El Callejón, c/ Guzmán el Bueno 18";
-asistentesEvt = "Eduardo Albarracín, Facundo Giménez Mena,Carlos Hornillos Blasco,Rafael Ladrón de Guevara,Francisco Llorca,Antonino de Mora Montes,Juan Diego Pérez Pérez,José María Pertusa,Ramón Peyró, Bartolomé Sánchez,Luis Torres,Clemente Úbeda";
+asistentesEvt = "Eduardo Albarracín;Facundo Giménez Mena;Carlos Hornillos Blasco;Rafael Ladrón de Guevara;Francisco Llorca;Antonino de Mora Montes;Juan Diego Pérez Pérez;José María Pertusa;Ramón Peyró;Bartolomé Sánchez;Luis Torres;Clemente Úbeda";
 fotosEvt = "ElCallejon_Google.jpg,WhatsApp Image 2018-09-21 at 20.32.43.jpeg,Chapulines_2018-09-16 at 19.44.59.jpeg,WhatsApp Image 2018-09-21 at 13.17.44.jpeg,WhatsApp Image 2018-09-21 at 13.21.14.jpeg,WhatsApp Image 2018-09-21 at 20.32.33(1).jpeg,WhatsApp Image 2018-09-21 at 20.32.33.jpeg,WhatsApp Image 2018-09-21 at 20.32.34.jpeg,WhatsApp Image 2018-09-21 at 20.32.43(1).jpeg,WhatsApp Image 2018-09-21 at 20.32.44.jpeg";
 
 menuEvt = "José María nos obsequió con los exóticos Chapulines.<br/>" +
